8a759847601a67d2ed1b55d8ebc4876ba1878d0d
Muhammad Hassan <muhammmadhasssan110786@gmail.com>
2023-11-09 19:58:08 +0500

    sixth class

---
+++ js/sixth class/app.js
@@ -0,0 +1,62 @@
+// if (city === cityName[0]) {
+//   alert("karachi is great city");
+// } else if (city === cityName[1]) {
+//   alert("punjab is great city");
+// } else if (city === cityName[2]) {
+//   alert("islambad is great city");
+// } else if (city === cityName[3]) {
+//   alert("lahore is great city");
+// } else {
+//   alert("city should in Pakistan");
+// }
+
+// var fav = "favourite";
+// var city = prompt("Enter your " + fav + " city name");
+// var cityName = ["karachi", "punjab", "islamabad", "lahore"];
+
+// console.log(cityName.length, city.length);
+
+// break: it terminates the loop
+// var flag = true;
+
+// for (var i = 0; i < cityName.length; i++) {
+//   console.log(city, cityName[i], flag);
+//   if (city === cityName[i]) {
+//     alert(city + " is great city");
+//     flag = false;
+//     break;
+//   }
+// }
+
+// console.log(flag);
+// if (flag) {
+//   alert("city should in Pakistan");
+// }
+
+// var firstArr = ["popat", "patrakar"];
+// var lastArr = ["lal", "hara", "pila", "nila"];
+
+// for (var i = 0; i < firstArr.length; i++) {
+//   for (var x = 0; x < lastArr.length; x++) {
+//     console.log(firstArr[i] + lastArr[x]);
+//   }
+// }
+
+// Case Changing Methods
+// toUpperCase();
+// toLowerCase();
+
+// var first = "MUHAMMMAD";
+// var last = "hassan";
+// console.log(first.toLowerCase());
+// console.log(last.toUpperCase());
+
+// javascript is case sensitive
+
+console.log("Hassan" === "hassan");
+
+// var first = "MUHAMMAD";
+// var copy = first.slice(2, first.length);
+// console.log(copy);
+// console.log(first.length);
+// console.log(first[7]);
