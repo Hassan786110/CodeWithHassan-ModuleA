6380fa6ac46b844f6d056547e904c0aab15fff34
Muhammad Hassan <muhammmadhasssan110786@gmail.com>
2023-11-24 21:52:25 +0500

    tenth class

---
+++ js/tenth class/app.js
@@ -0,0 +1,33 @@
+// function
+
+// functions increase re usability of code
+// Saves our time and effort
+
+// greetUser("ali"); argument
+// function greetUser(name) { parameter
+
+// var name1 = "hassan";
+// var name2 = "ali";
+// alert("Hello " + name1);
+// alert("Good Morning " + name1);
+// alert("Hello " + name2);
+// alert("Good Morning " + name2);
+
+// greetUser("hassan");
+// greetUser("ali");
+
+// function greetUser(name) {
+//   console.log(name);
+
+//   alert("Hello " + name);
+//   alert("Good Morning " + name);
+// }
+
+// function getDay(date) {
+//   var date = new Date(date);
+//   var daysArr = ["Sun", "Mon", "Tue", "Wed", "thu", "Fri", "sat"];
+//   var getDay = daysArr[date.getDay()];
+//   return getDay;
+// }
+// console.log(getDay("2023-11-10"));
+// console.log(getDay("2023-11-11"));
